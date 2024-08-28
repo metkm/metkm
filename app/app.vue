2bb1acbe54d50beb34e8d7508c316eca40692ab3
metkm <metinkorkmaz417@gmail.com>
2024-08-28 23:51:58 +0300

    camera look at the center

--- app/app.vue
+++ app/app.vue
@@ -9,11 +9,11 @@ extendTres({ OrbitControls })
     window-size
     clear-color="black"
   >
-    <TheCamera />
+    <TresPerspectiveCamera :args="[45, 1, 0.1, 1000]" />
     <TheOrbitControls />
 
     <Suspense>
-      <TheImages />
+      <TheImages ref="group" />
     </Suspense>
   </TresCanvas>
 </template>
--- app/components/TheImages.vue
+++ app/components/TheImages.vue
@@ -1,36 +1,47 @@
 <script setup lang="ts">
-import { SRGBColorSpace } from 'three'
+import type { Group } from 'three'
+import { Box3, SRGBColorSpace, Vector3 } from 'three'
 import { images } from '~/constants'
 
+const { camera } = useTresContext()
 const _textures = await useTexture(images)
 const textures = fillArray(_textures, 100)
+
+const group = shallowRef<Group>()
 
 textures.forEach((texture) => {
   texture.colorSpace = SRGBColorSpace
 })
+
+const stop = watch(group, () => {
+  if (!group.value || !camera.value) return
+
+  const box = new Box3()
+  const size = new Vector3()
+
+  box.setFromObject(group.value)
+  box.getSize(size)
+
+  const centerX = size.x / 2
+  const centerY = -size.y / 2
+
+  camera.value.position.x = centerX
+  camera.value.position.y = centerY
+  camera.value.position.z = 20
+
+  camera.value.lookAt(centerX, centerY, 0)
+
+  stop()
+})
 </script>
 
 <template>
-  <TresGroup>
+  <TresGroup ref="group">
     <TheImage
       v-for="(texture, index) in textures"
       :key="texture.id"
       :index="index"
       :texture="texture"
     />
-    <!-- <TresMesh
-      v-for="(texture, index) in textures"
-      :key="texture.id"
-      :position="[
-        (index * 4) - (Math.floor(index / 10) * (10 * 4)),
-        -Math.floor(index / 10) * 4,
-        0,
-      ]"
-      @pointer-enter="handlePointerEnter"
-      @pointer-leave="handlePointerLeave"
-    >
-      <TresBoxGeometry :args="[4, 4, 0]" />
-      <TresMeshBasicMaterial :map="texture" />
-    </TresMesh> -->
   </TresGroup>
 </template>
