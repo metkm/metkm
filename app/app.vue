9ddf9665d6dcc844c53401b5f5eb7aae03b3b425
metkm <metinkorkmaz417@gmail.com>
2024-08-29 17:39:22 +0300

    remove stuff that won't be used

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,6 @@
 <script setup lang="ts">
-import { AxesHelper } from 'three'
-
-extendTres({
-  AxesHelper,
-})
 </script>
 
 <template>
-  <TresCanvas
-    window-size
-  >
-    <TresPerspectiveCamera
-      :args="[45, 1, 0.1, 1000]"
-      :position="[-6, 0, 8]"
-      :look-at="[0, 0, 0]"
-    />
-    <TresAxesHelper />
-    <!-- <TheOrbitControls /> -->
-
-    <Suspense>
-      <TheImages
-        ref="group"
-      />
-    </Suspense>
-  </TresCanvas>
+  <p>hello world</p>
 </template>
