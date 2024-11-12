d3ce4bdc3e1d6830dea75bd96b19bc4e0223c482
metkm <metinkorkmaz417@gmail.com>
2024-11-12 13:10:39 +0300

    wip

--- app/app.vue
+++ app/app.vue
@@ -20,89 +20,54 @@ const items = ref(cardsWithIds)
 </script>
 
 <template>
-  <main
-    class="grid grid-rows-[1fr_0fr] lg:grid-cols-[1fr_0fr] lg:max-h-lvh min-h-lvh overflow-hidden transition-all duration-500"
-    :class="{ 'lg:grid-cols-[1fr_1fr] lg:!grid-rows-1 !grid-rows-[1fr_1fr]': selected }"
-  >
-    <TheCanvas />
+  <main class="flex flex-col justify-center grow h-screen">
+    <div class="absolute inset-0 flex flex-col justify-center pointer-events-none">
+      <div class="bg-[#2f2015] h-[70%]" />
+    </div>
+    <div class="absolute inset-0 z-50 pointer-events-none">
+      <div class="absolute inset-0 w-full h-full pattern opacity-40" />
+      <div class="absolute inset-0 w-full h-full dark-round-overlay" />
+    </div>
 
-    <div class="flex flex-col justify-center items-center pt-4 overflow-hidden max-w-screen gap-4 lg:gap-8">
-      <TransitionGroup
-        move-class="transition-all"
-        leave-active-class="absolute transition-all duration-500"
-        enter-active-class="transition-all duration-500"
-        leave-to-class="!translate-y-14 !opacity-0"
-        enter-from-class="!translate-y-14 !opacity-0"
+    <ul class="flex items-center mx-auto z-40">
+      <li
+        :style="{
+          backgroundImage: `url(https://pbs.twimg.com/media/GaPo3bpbUAMt74u?format=jpg&name=large)`,
+        }"
+        class="aspect-[1.1/2] h-[calc(100vh*0.85)] bg-center bg-cover shadow-glow rounded-lg transition-all hover:saturate-150"
       >
-        <div
-          v-if="selected"
-          :key="1"
-        >
-          <TheCard :item="selected" />
-        </div>
-
-        <TheCards
-          :key="2"
-          v-model="selected"
-          v-model:items="items"
-        />
-      </TransitionGroup>
-    </div>
-
-    <div class="flex items-center min-w-0 min-h-0 transition-all">
-      <Transition
-        leave-active-class="transition-all"
-        enter-active-class="transition-all"
-        leave-to-class="opacity-0"
-        enter-from-class="opacity-0"
-      >
-        <section
-          v-if="selected"
-          class="text-neutral-100 flex flex-col gap-2 lg:gap-4 justify-between rounded-lg p-2 lg:p-4 w-full aspect-video"
-        >
-          <div>
-            <h1>
-              {{ selected.title }}
-            </h1>
-            <p class="text-neutral-400 text-sm">
-              {{ selected.smallDescription }}
-            </p>
-          </div>
-
-          <p class="text-neutral-100">
-            {{ selected.description }}
-          </p>
-
-          <img
-            :src="selected.image"
-            class="rounded-lg max-h-full shadow object-cover"
-          >
-
-          <a
-            :href="selected.url"
-            target="_blank"
-            class="hover:bg-neutral-200 w-max p-1 rounded-lg"
-          >
-            <IconGithub />
-          </a>
-        </section>
-      </Transition>
-    </div>
-
-    <div class="lg:absolute left-4 bottom-4 p-4 font-bold text-on-primary font-serif flex flex-col self-start">
-      <p>
-        Me:
-      </p>
-      <a href="https://github.com/metkm">Github</a>
-      <a href="https://www.linkedin.com/in/metkm/">LinkedIn</a>
-    </div>
+        <p>content</p>
+      </li>
+    </ul>
   </main>
 </template>
 
 <style>
 html {
   font-family: Inter;
-  background-color: #EADBC8;
-  color: white;
+  background-color: #ffcaa1;
+  min-height: 100vh;
+}
+
+html,
+body,
+#__nuxt {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+}
+
+.pattern {
+  background-image: url("./assets/pattern.png");
+  background-repeat: repeat;
+  backdrop-filter: blur(5px) saturate(3);
+}
+
+.dark-round-overlay {
+  background: radial-gradient(ellipse at center, transparent 0%, rgba(0, 0, 0, 0.8));
+}
+
+.shadow-glow {
+  box-shadow: 0 0 200px rgba(82, 18, 18, 0.5);
 }
 </style>
