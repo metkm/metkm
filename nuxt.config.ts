bf10252db10f34dabee218d3eda2d0face10964e
metkm <metinkorkmaz417@gmail.com>
2025-02-04 14:10:33 +0300

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/eslint',
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt',
-    '@nuxt/fonts',
-    '@tresjs/nuxt',
-    '@vueuse/nuxt',
-  ],
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-  },
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'en',
-      },
-      link: [
-        {
-          rel: 'icon',
-          type: 'image/svg+xml',
-          href: '/favicon.svg',
-        },
-      ],
-    },
-  },
-  tailwindcss: {
-    configPath: './tailwind.config.ts',
-  },
-  future: {
-    compatibilityVersion: 4,
-  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
 })
