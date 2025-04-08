16209e8a77f9b8d3217e19ea8d5c31357ec78792
metkm <metinkorkmaz417@gmail.com>
2025-04-08 20:55:09 +0300

    fade and stuff

--- app/app.vue
+++ app/app.vue
@@ -14,8 +14,18 @@ useSeoMeta({
 </script>
 
 <template>
-  <main class="flex flex-col gap-24 p-8 py-12 min-h-screen">
-    <div class="flex items-center group transition-all gap-2 max-w-xl mx-auto slide-enter">
+  <main class="slide-enter-content flex flex-col gap-24 p-8 py-12 min-h-screen bg-(--ui-bg)">
+    <div class="fixed inset-0 !pointer-events-none">
+      <TresCanvas class="h-full w-full !pointer-events-none">
+        <TresOrthographicCamera
+          :position="[0, 0, 1]"
+        />
+
+        <TheNoise />
+      </TresCanvas>
+    </div>
+
+    <div class="flex items-center group transition-all gap-2 max-w-xl mx-auto">
       <h1 class="font-bold text-4xl">
         Metin
       </h1>
@@ -172,146 +182,11 @@ useSeoMeta({
         Some projects I've worked on
       </p>
 
-      <TheProjects />
+      <ClientOnly>
+        <Transition name="fade">
+          <TheProjects />
+        </Transition>
+      </ClientOnly>
     </section>
-
-    <div class="fixed inset-0 !pointer-events-none -z-50 slide-enter">
-      <TresCanvas class="h-full w-full !pointer-events-none">
-        <TresOrthographicCamera
-          :position="[0, 0, 1]"
-        />
-
-        <TheNoise />
-      </TresCanvas>
-    </div>
   </main>
 </template>
-
-<!-- <script setup lang="ts">
-import { projects } from './projects'
-
-useSeoMeta({
-  title: 'Metin Korkmaz',
-  description: 'Metin\'s portfolio',
-  ogTitle: 'Metin Korkmaz',
-  ogImage: '/preview.webp',
-  ogDescription: 'Metin\'s portfolio',
-  ogUrl: 'https://metkm.win/',
-  twitterCard: 'summary_large_image',
-  twitterTitle: 'Metin Korkmaz',
-  twitterDescription: 'Metin\'s portfolio',
-  twitterImage: '/preview.webp',
-})
-
-const carousel = useTemplateRef('carousel')
-
-const selectedProjectIndex = ref(0)
-
-const project = computed(() => projects[selectedProjectIndex.value]!)
-
-const selectNextProject = () => {
-  carousel.value?.emblaApi?.scrollNext()
-}
-
-const selectPrevProject = () => {
-  carousel.value?.emblaApi?.scrollPrev()
-}
-
-const onCarouselSelect = () => {
-  const selectedIndex = carousel.value?.emblaApi?.selectedScrollSnap()
-  if (selectedIndex === undefined) return
-
-  selectedProjectIndex.value = selectedIndex
-}
-
-onMounted(() => {
-  carousel.value?.emblaApi?.on('select', onCarouselSelect)
-})
-</script>
-
-<template>
-  <main class="flex bg-background">
-    <svg class="absolute inset-0 z-50 pointer-events-none">
-      <filter id="grainy">
-        <feTurbulence
-          type="fractalNoise"
-          baseFrequency="0.6"
-        />
-        <feComposite
-          operator="in"
-          in2="SourceGraphic"
-          result="monoNoise"
-        />
-      </filter>
-    </svg>
-
-    <div class="flex flex-col gap-4 min-h-screen max-h-screen h-screen overflow-hidden z-10">
-      <div class="flex flex-wrap gap-4 items-end max-w-7xl w-full mx-auto justify-between text-text-primary font-bold p-8 pb-0">
-        <p class="text-lg lg:text-4xl font-stretch-condensed bg-black/20 lg:bg-transparent px-2 lg:px-0">
-          [ {{ project.title }}. ]
-        </p>
-
-        <p class="text-2xl lg:text-6xl">
-          <span class="text-xl">made with</span>
-          {{ project.madeWith }}
-        </p>
-      </div>
-
-      <UCarousel
-        ref="carousel"
-        v-slot="{ item }"
-        :items="projects"
-        class="max-h-full flex-1 overflow-hidden"
-        :contain-scroll="false"
-        :ui="{
-          item: 'h-full basis-[90%] relative',
-          container: 'h-full',
-          viewport: 'h-full',
-        }"
-      >
-        <div class="flex pr-8 pb-8 before:absolute before:bg-text-primary before:left-1/12 before:top-1/12 before:right-0 before:bottom-0 before:flex h-full">
-          <img
-            :src="item.image"
-            class="h-full w-full object-cover z-10"
-          >
-        </div>
-      </UCarousel>
-
-      <div class="flex flex-wrap gap-4 items-center justify-between p-8 pt-0 max-w-7xl w-full mx-auto">
-        <p class="underline underline-offset-8 lg:text-xl font-semibold text-text-description">
-          {{ project.description }}
-        </p>
-
-        <div class="flex items-center gap-2  text-black">
-          <UiButton
-            icon="mdi:github"
-            href="https://github.com/metkm"
-          />
-
-          <UiButton
-            icon="mdi:linkedin"
-            href="https://www.linkedin.com/in/metkm/"
-          />
-
-          <UiButton
-            icon="mdi:arrow-left"
-            @click="selectPrevProject"
-          />
-
-          <UiButton
-            icon="mdi:arrow-right"
-            @click="selectNextProject"
-          />
-        </div>
-      </div>
-    </div>
-
-    <TresCanvas class="!absolute inset-0 pointer-events-none">
-      <TresOrthographicCamera
-        :position="[0, 0, 1]"
-      />
-
-      <TheNoise />
-    </TresCanvas>
-  </main>
-</template> -->
