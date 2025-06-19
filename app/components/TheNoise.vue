4438949f3537646bcca1783e31d6df151e24ee00
metkm <metinkorkmaz417@gmail.com>
2025-06-19 20:24:46 +0300

    reactive width, height

--- app/components/TheNoise.vue
+++ app/components/TheNoise.vue
@@ -3,19 +3,32 @@ import { Vector2, Vector3 } from 'three'
 import commonVertex from '~/shader/commonVertex.glsl?raw'
 import noiseFrag from '~/shader/noiseFragment.glsl?raw'
 
-const width = window.innerWidth
-const height = window.innerHeight
+const width = ref(window.innerWidth)
+const height = ref(window.innerHeight)
 
 const { onLoop } = useRenderLoop()
 
 const uniforms = {
-  resolution: { value: new Vector2(width, height) },
+  resolution: { value: new Vector2(width.value, height.value) },
   targetColor: { value: new Vector3(0, 0, 0) },
   time: { value: 0 },
 }
 
 onLoop(({ elapsed }) => {
   uniforms.time.value = elapsed / 5
+})
+
+onMounted(() => {
+  window.addEventListener('resize', () => {
+    const val = uniforms.resolution.value as Vector2
+
+    const w = window.innerWidth
+    const h = window.innerHeight
+
+    val.set(w, h)
+    width.value = w
+    height.value = h
+  })
 })
 </script>
 
