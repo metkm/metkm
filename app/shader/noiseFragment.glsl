90f11d1dbed905e7320a90ca119c379ac19bfc2e
metkm <metinkorkmaz417@gmail.com>
2025-06-19 20:14:43 +0300

    little cleanup

--- app/shader/noiseFragment.glsl
+++ app/shader/noiseFragment.glsl
@@ -60,17 +60,24 @@ void main() {
   color = vec3(billow);
 
   // One line
-  color = smoothstep(0.001, 0.005, color);
+  // color = smoothstep(0.001, 0.005, color);
 
   // Multiple lines
   // color = round(color / 0.10) * 0.10;
 
   // Multiple lines 2
   float st = round(billow / 0.08) * 0.08;
-  float st_low = st - st;
-  float st_high = st + st;
-  float st_mid = (st_low + st_high) / 4.0; // or divide by 2
-  color = vec3(step(billow, st_mid + 0.035));
+  color = vec3(
+    step(
+      billow,
+      st / 4.0 + 0.035
+    )
+  );
+
+  // float st_low = st - st;
+  // float st_high = st + st;
+  // float st_mid = (st_low + st_high) / 4.0; // or divide by 2
+  // color = vec3(step(billow, st_mid + 0.035));
 
   color = mix(targetColor, vec3(0.2), color);
   gl_FragColor = vec4(color, 0.1);
